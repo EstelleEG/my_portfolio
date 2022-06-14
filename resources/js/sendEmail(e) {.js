93441d47654a9d40cc7c384dbaa9298f969f7f71
Estelle <estellegregoire0689@gmail.com>
2022-06-14 13:38:21 +0200

    updates

--- resources/js/sendEmail(e) {.js
+++ resources/js/sendEmail(e) {.js
@@ -1,44 +1,44 @@
-sendEmail(e) {
- /* Je vérifie si mon formulaire est valide + regex de l'email. */
-    if (this.checkForm() && this.emailIsValid()) {
-        try {
-            emailjs
-                .sendForm(
-                    "service_w94zr4t",
-                    "template_ah0hnzh",
-                    e.target,
-                    "lTg6uB3SSqdJZhtqN",
-                    {
-                        name: this.name,
-                        email: this.email,
-                        message: this.message,
-                    }
-                )
+// sendEmail(e) {
+//  /* Je vérifie si mon formulaire est valide + regex de l'email. */
+//     if (this.checkForm() && this.emailIsValid()) {
+//         try {
+//             emailjs
+//                 .sendForm(
+//                     "service_vbfmzcj",
+//                     "template_zm0k9yn",
+//                     e.target,
+//                     "4riFHalEW_nXH8pM7",
+//                     {
+//                         name: this.name,
+//                         email: this.email,
+//                         message: this.message,
+//                     }
+//                 )
 
-/* Retour de l'api : l'email a bien été envoyé */
-                .then(
-                    (result) => {
+// /* Retour de l'api : l'email a bien été envoyé */
+//                 .then(
+//                     (result) => {
 
-/*Tu peux mettre ton code ici pour dire que l'email est bien envoyé.*/
-                        this.hideForm = true;
-                        console.log("Email envoyé: " + result);
-                    },
+// /*Tu peux mettre ton code ici pour dire que l'email est bien envoyé.*/
+//                         this.hideForm = true;
+//                         console.log("Email envoyé: " + result);
+//                     },
 
-/* Retour de l'api : erreur */
-                    (error) => {
+// /* Retour de l'api : erreur */
+//                     (error) => {
 
-                        console.log("Une erreur est survenue: " + error.text);
-                        this.messageError = "Une erreur inconnue est survenue.";
-                    }
-                );
-        } catch (error) {
-            this.messageError =
-                "Une erreur inconnue est survenue, veuillez ressayer plus tard.";
-        }
+//                         console.log("Une erreur est survenue: " + error.text);
+//                         this.messageError = "Une erreur inconnue est survenue.";
+//                     }
+//                 );
+//         } catch (error) {
+//             this.messageError =
+//                 "Une erreur inconnue est survenue, veuillez ressayer plus tard.";
+//         }
         
-// Reset du formulaire
-        this.name = "";
-        this.email = "";
-        this.message = "";
-    }
-}
+// // Reset du formulaire
+//         this.name = "";
+//         this.email = "";
+//         this.message = "";
+//     }
+// }
